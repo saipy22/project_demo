31c7e01e68abede345ba4492da3d69c1c3c95042
Saikiran <60926269+saikiran353@users.noreply.github.com>
2021-09-11 17:25:45 +0530

    Update sai_py1.py
    added one more user condition 'yep' to it.

--- sai_py1.py
+++ sai_py1.py
@@ -1,7 +1,7 @@
 print('hello_saipy')
 
 user = input('hey do u wanna play it: ')
-if user in ('yes', 'y', 'ok'):
+if user in ('yes', 'y', 'ok', 'yep'):
     print('here we go, have it')
 else:
     print('ok. take rest for now')
